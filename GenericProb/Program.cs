49fd70ea4e4dad36d68289796ad9da55371f81d6
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-09-18 16:21:22 +0530

    Add MaxNumberFirstNumber

--- GenericProb/Program.cs
+++ GenericProb/Program.cs
@@ -6,5 +6,5 @@ int a = 10;
 int b = 20;
 int c = 30;
 MaxNumber maxNumber = new MaxNumber();
-Console.WriteLine(maxNumber.PrintMaxNumber(a,b,c));
+Console.WriteLine(maxNumber.PrintMaxNumber(c,a,b));
 Console.ReadLine();
