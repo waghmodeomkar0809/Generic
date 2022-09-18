f2c3866f5d323de4ecb7b743132a559e8e48d932
waghmodeomkar0809 <waghmodeomkar975@gmail.com>
2022-09-18 16:01:50 +0530

    Add MaxNumber

---
+++ GenericProb/MaxNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProb
+{
+    public class MaxNumber
+    {
+        public int PrintMaxNumber(int num1, int num2, int num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0
+              || num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) >= 0
+              || num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0)
+                {
+                return num1;
+                 }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0
+             || num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) >= 0
+             || num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+                {
+                return num2;
+                 }
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0
+             || num3.CompareTo(num1) >= 0 && num3.CompareTo(num2) >= 0
+             || num3.CompareTo(num1) > 0 && num3.CompareTo(num2) >= 0)
+                {
+                return 3;
+                }
+            return num1;
+        }
+       
+
+       
+    }
+}
---
+++ GenericProb/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericProb;
+using System;
+Console.WriteLine("Hello, World!");
+int a = 10;
+int b = 20;
+int c = 30;
+MaxNumber maxNumber = new MaxNumber();
+Console.WriteLine(maxNumber.PrintMaxNumber(a,b,c));
+Console.ReadLine();
